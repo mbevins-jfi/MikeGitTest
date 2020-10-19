f8b6cefae2df98e613e6627025b1d39245b647c0
Mike Bevins <michael.bevins@jonasfitness.com>
2020-10-19 12:45:46 -0400

    Added Hello World 2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace MikeGitTest
         static void Main(string[] args)
         {
             Console.Write("Hello World");
+            Console.Write("Hello World 2");
         }
     }
 }
