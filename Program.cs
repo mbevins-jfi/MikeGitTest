0174471cd52be69da6651f17604bae5c6a182652
Mike Bevins <michael.bevins@jonasfitness.com>
2020-10-19 12:33:49 -0400

    Updated output message

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MikeGitTest
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            Console.Write("Hello World");
         }
     }
 }
